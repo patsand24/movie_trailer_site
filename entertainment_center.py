483f4d5349284c0b9774b75377657fedcccdae6d
Patrick Sandoval <patsand24csu@yahoo.com>
2017-02-24 17:34:59 -0800

    Cleaned up code

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,39 +1,27 @@
 import media
 import fresh_tomatoes
 
-rogue_one = media.Movie("Rogue One",
-                        "133 Minutes",
-                        "Storyline",
+rogue_one = media.Movie("Rogue One", "133 Minutes", "Storyline",
                         "http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2016/10/rogueone_onesheetA.jpg",
                         "https://www.youtube.com/watch?v=frdj1zb9sMY")
 
-step_brothers = media.Movie("Step Brothers",
-                            "98 Minutes",
-                            "Storyline",
+step_brothers = media.Movie("Step Brothers", "98 Minutes", "Storyline",
                             "http://cdn.pcwallart.com/images/step-brothers-poster-wallpaper-1.jpg",
                             "https://www.youtube.com/watch?v=ANjenc4W1_Q")
 
-get_him_to_the_greek = media.Movie("Get Him to the Greek",
-                                   "114 Minutes",
-                                   "Storyline",
+get_him_to_the_greek = media.Movie("Get Him to the Greek", "114 Minutes", "Storyline",
                                    "http://cdn2-www.comingsoon.net/assets/uploads/1970/01/file_540995_greekposter.jpg",
                                    "https://www.youtube.com/watch?v=N6ixkr0-qvo")
 
-across_the_universe = media.Movie("Across the Universe",
-                                  "133 Minutes",
-                                  "Storyline",
+across_the_universe = media.Movie("Across the Universe", "133 Minutes", "Storyline",
                                   "http://www.impawards.com/2007/posters/across_the_universe.jpg",
                                   "https://www.youtube.com/watch?v=43aLbo-Y_W0")
 
-the_revenant = media.Movie("The Revenant",
-                           "156 Minutes",
-                           "Storyline",
+the_revenant = media.Movie("The Revenant", "156 Minutes", "Storyline",
                            "https://s-media-cache-ak0.pinimg.com/736x/01/ff/38/01ff38f1fa846d599230cbf5b89b8ea3.jpg",
                            "https://www.youtube.com/watch?v=QRfj1VCg16Y")
 
-spotlight = media.Movie("Spotlight",
-                        "129 Minutes",
-                        "Storyline",
+spotlight = media.Movie("Spotlight", "129 Minutes", "Storyline",
                         "http://www.impawards.com/2015/posters/spotlight_ver2.jpg",
                         "https://www.youtube.com/watch?v=Zg5zSVxx9JM")
 
--- media.py
+++ media.py
@@ -1,20 +1,22 @@
 import webbrowser
 
+# Parent Class used in case future Child Class is added later
 class Video():
-    def __init__(self, movie_title, movie_duration):
-        self.title = movie_title
-        self.duration = movie_duration
+    def __init__(self, video_title, video_duration):
+        self.title = video_title
+        self.duration = video_duration
 
+#Child of Video
 class Movie(Video):
     """ This class provides a way to store movie related information"""
 
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-
-    def __init__(self, movie_title, movie_duration, movie_storyline, poster_image, trailer_youtube):
-        Video.__init__(self, movie_title, movie_duration)
+    def __init__(self, video_title, video_duration, movie_storyline,
+                 poster_image, trailer_youtube):
+        Video.__init__(self, video_title, video_duration)   #Inherited from Parent
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+# Will show the trailer to make sure it is opertaing properly
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
